a362e060529a4c36b4f00d7fc333cbc77f198d80
cristiana.casanova <cristiana.casanova@optit.net>
2024-03-14 14:39:40 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,77 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CubeComponent } from './cube/cube.component';
-import { ModelComponent } from './model/model.component';
+
+
+
+
+
+
+
+// Apex chart
+
+import { RePasswordComponent } from './landing-page/auth/re-password/re-password.component';
+import { SigninComponent } from './landing-page/auth/signin/signin.component';
+import { SignupComponent } from './landing-page/auth/signup/signup.component';
+
+import { DatePipe } from '@angular/common';
+import { ChPasswordComponent } from './landing-page/auth/ch-password/ch-password.component';
+import { WasmService } from './services/wasm.service';
+import { ErrorInterceptor } from './account/error.interceptor';
+import { JwtInterceptor } from './account/jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent,
-    ModelComponent
+    SigninComponent,
+    RePasswordComponent,
+    ChPasswordComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot([], {}),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  providers: [
+    DatePipe,
+    WasmService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '663990587528-hmu2scpu3t5m3eablhkmb62uq57r1i8c.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
