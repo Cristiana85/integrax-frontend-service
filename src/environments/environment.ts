7c2477b9c57683b6df6b93050bdc058bb74c9d7a
cristiana.casanova <cristiana.casanova@optit.net>
2024-03-14 15:10:02 +0100

    WIP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiRoot: 'http://localhost:5052'
+  //apiRoot: 'http://localhost:5052'
+  apiRoot: 'https://integrax-backend-gcl-3hkbcb6pua-uc.a.run.app:8080'
 };
